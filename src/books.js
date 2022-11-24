b44efa3623479e23276e9f07d43d137a1a22cf88
anisahmw <109682880+anisahmw@users.noreply.github.com>
2022-11-25 03:00:18 +0530

    Added product page

---
+++ src/books.js
@@ -0,0 +1,56 @@
+import Spidey1 from "./images/spiderman1.png";
+import Spidey2 from "./images/spiderman2.png";
+import Spidey3 from "./images/spiderman3.png";
+import Spidey4 from "./images/spiderman4.png";
+import Dictionary from "./images/oxford.png";
+
+export const books = {
+  Comics: [
+    {
+      name: "Traditional Silk Saree with Blouse for Women",
+      price: 1.99,
+      rating: 4,
+      image: Spidey1,
+      about:
+        "India was one of the first countries in the world to start producing textiles, and such product was so central to its culture that in ancient Greece the word India indicated cotton. Silk and cotton weaved textiles and good brocade can be bought by meter or as ready-made garments",
+    },
+    {
+      name: "Madhubani Stick figure Painting",
+      price: 9.99,
+      rating: 5,
+      image: Spidey2,
+      about:
+        "Madhubani uses only natural dyes and pigments and every painting is characterized by strong geometric patterns. The content can be adapted to a particular occasion such as birth, marriage, or festivals. Some designs may also depict scenes from the royal court or deities from the ancient stories.",
+    },
+    {
+      name: "Indian bamboo and cane collection set",
+      price: 2.99,
+      rating: 1,
+      image: Spidey3,
+      about:
+        "The northeastern states are widely known for their bamboo and cane products. They create objects for everyday use,  like baskets for tea pickers, hats, etc.,  as well as a variety of boxes, trays, baskets, and containers. Of course, they also produce excellent furniture, to be sold both locally and internationally.",
+    },
+    {
+      name: "Indian stoneware plates with stonework",
+      price: 4.99,
+      rating: 3,
+      image: Spidey4,
+      about:
+        "Pietra dura, known as parchinkari in the Indian subcontinent, indicates the inlay technique of using cut and fitted stones in order to create images. After the work is assembled, the stonework is glued stone-by-stone. Stability is obtained by grooving the undersides of the stones so that they can interlock",
+    },
+  ],
+  Dictionaries: [
+    {
+      name: "Macrame Leaf Wall Hanging",
+      price: 2.29,
+      rating: 5,
+      image: Dictionary,
+      about:
+        "This beautiful Green Leaves Macrame wall hanging is a truly piece of art that would be a perfect decoration touch for Boho Home Decor, Scandinavian Decor or in any corner in your home. It also could be a perfect idea for New Home Gift, Bestfriend Gift, Housewarming Gift."
+    },
+  ],
+  Novels: [],
+  Fantasy: [],
+  Horror: [],
+  Adventure: [],
+};
